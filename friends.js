c1443da665d112c827ef1f157147d5e252eb9ac6
Patt Sookmark <pijiya@sookmark.com>
2022-09-22 15:54:18 -0600

    Add final

--- friends.js
+++ friends.js
@@ -1,9 +1,18 @@
 var i=[];
-for (var i=0; i<=100; i++) {
-  if (i >=30)
 while (i.length < 8){
   var r = Math.floor(Math.random() * 100)+1;
   if (i.indexOf(r) === -1) i.push(r);
-    }
+
+}
+
+for (var i=0; i<=100; i++) {
+  if (i >=30){
     console.log(i);
   }
+}
+
+for (var i=0; i<=100; i++) {
+  if (i <=60){
+    console.log(i);
+  }
+}
