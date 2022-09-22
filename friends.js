2dbaa9e30e6bffed27ef5331c6aef2a1bb207a7a
Patt Sookmark <pijiya@sookmark.com>
2022-09-22 15:50:35 -0600

    Initial commit

---
+++ friends.js
@@ -0,0 +1,9 @@
+var i=[];
+for (var i=0; i<=100; i++) {
+  if (i >=30)
+while (i.length < 8){
+  var r = Math.floor(Math.random() * 100)+1;
+  if (i.indexOf(r) === -1) i.push(r);
+    }
+    console.log(i);
+  }
